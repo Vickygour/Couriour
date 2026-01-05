41e42ddc970cef57d36818e9579a35f7f861a4e6
Vickygour <vickygour9868@gmail.com>
2026-01-05 15:36:56 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+
+
+
+
+function App() {
+  return (
+    <>
+      hi
+    </>
+  );
+}
+
+export default App;
